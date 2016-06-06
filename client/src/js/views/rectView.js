156aee6796bbf3586a5239321efe8e36858ab585
Rene <devproren@gmail.com>
2016-06-05 21:46:43 -0700

    having trouble with loading libraries

--- client/src/js/views/rectView.js
+++ client/src/js/views/rectView.js
@@ -1,9 +1,14 @@
 import Backbone from 'backbone';
+
+import 'd3';
+// import d3_drag from 'd3-drag';
+// import d3_selection from 'd3-selection';
 //
 // TODO figure out why we need to require these libraries
 // Doesn't webpack handle es6 import? Maybe it can't handle jquery-ui format?
 var $ = require('jquery');
 require('jquery-ui/ui/widgets/draggable.js');
+
 
 var rectView = Backbone.View.extend({
 
@@ -15,11 +20,12 @@ var rectView = Backbone.View.extend({
     this.model.on('change', this.render, this);
     // bind drag event handlers
     // http://jqueryui.com/draggable/#events
-    $(rect).draggable({
-      start: this.onDragStart,
-      drag: this.onDrag.bind(this),
-      stop: this.onDragStop
-    });
+    // $(rect).draggable({
+    //   start: this.onDragStart,
+    //   drag: this.onDrag.bind(this),
+    //   stop: this.onDragStop
+    // });
+
 
     this.setElement(rect); 
   },
@@ -30,18 +36,29 @@ var rectView = Backbone.View.extend({
       .attr('y', this.model.attributes.y)
       .attr('width', this.model.attributes.width)
       .attr('height', this.model.attributes.height);
+
+    var drag = d3.drag()
+      .on('start', this.handleDragStart)
+      .on('drag', this.handleDrag)
+      .on('end', this.handleDragEnd);
+
+    // drag.call(d3_selection.select('rect'));
+    // drag.call(this.$el);
+
     return this;
   },
 
-  onDragStart: function(e) {
+  handleDragStart: function(e) {
     console.log('drag start');
     // console.log(e);
   },
   
   // http://stackoverflow.com/questions/1108480/svg-draggable-using-jquery-and-jquery-svg#6166850
-  onDrag: function(e, ui) {
+  handleDrag: function(x, i) {
     // console.log(e);
-    // Note: `this` will refer to the element itself, not the model
+    // NOTE: `this` will refer to the element itself, not the model
+    // - walk through the d3-drag and d3-dispatch library to double check what the value of `this` is.
+    //
     this.model.set({
       x: ui.position.left,
       // TODO figure out why left and top position is off.  for now, quick fix to subtract width/2 for y value
@@ -50,7 +67,7 @@ var rectView = Backbone.View.extend({
   },
 
 
-  onDragStop: function(e) {
+  handleDragEnd: function(e) {
     console.log('drag end');
     console.log(e);
     // Stop event from propogating up to parent svg. Else, parent svg will handle event and unintentionally create a new rect shape on the board.
